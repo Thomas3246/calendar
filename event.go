05486f89e36fe881975d5398dbfb59a82d8e5dc2
Thomas3246 <anatoliy0642751@mail.ru>
2024-08-01 17:14:20 +0300

    get and set methods for the Event type

--- event.go
+++ event.go
@@ -1,6 +1,23 @@
 package calendar
 
+import (
+	"errors"
+	"unicode/utf8"
+)
+
 type Event struct {
-	Title string
+	title string
 	Date
+}
+
+func (e *Event) SetTitle(title string) error {
+	if utf8.RuneCountInString(title) > 30 {
+		return errors.New("INVALID TITLE")
+	}
+	e.title = title
+	return nil
+}
+
+func (e *Event) Title() string { //Get-method for titile
+	return e.title
 }
