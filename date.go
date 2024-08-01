b53fc62a02f633fcd4241b8c5bbcc6bcb12d1fa0
Thomas3246 <anatoliy0642751@mail.ru>
2024-08-01 11:39:26 +0300

    full code

--- date.go
+++ date.go
@@ -1,7 +1,33 @@
 package calendar
 
+import "errors"
+
 type Date struct {
 	Year  int
 	Month int
 	Day   int
+}
+
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("INVALID YEAR")
+	}
+	d.Year = year
+	return nil
+}
+
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("INVALID MONTH")
+	}
+	d.Month = month
+	return nil
+}
+
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("INVALID DAY")
+	}
+	d.Day = day
+	return nil
 }
